bdc6b3d68fe89d9bdee2d24e368eb34fcc0fdf11
Chainarong Kongpol <90338799+chainarong2546@users.noreply.github.com>
2024-11-04 15:05:36 +0700

    Add files via upload

---
+++ web-app/src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-white text-black">
+            <h1 className="text-5xl font-bold mb-4 text-center">Exam Checking</h1>
+            <p className="text-lg text-center max-w-2xl leading-relaxed">
+                ระบบตรวจสอบข้อสอบแบบมีหลายตัวเลือกด้วยเทคนิคการเรียนรู้เชิงลึก
+                วิชาการเขียนโปรแกรมคอมพิวเตอร์ สำหรับสาขาวิชาวิศวกรรมคอมพิวเตอร์
+                มหาวิทยาลัยเทคโนโลยีราชมงคลธัญบุรี
+            </p>
+            <p className="text-lg text-center max-w-2xl leading-relaxed mt-2">
+                Multiple choice exams checking system utilizing ML techniques: In case study of
+                computer programming course at Rajamangala University of Technology Thanyaburi.
+            </p>
+        </div>
+    );
+}
